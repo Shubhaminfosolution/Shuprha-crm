96b686d4c3bc1996e0173e4f7a843044c1ace4f0
Shubham More <shubhammore0512@gmail.com>
2026-03-21 19:34:51 +0530

    fix the is staff to false

--- Users/models.py
+++ Users/models.py
@@ -45,7 +45,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     role = models.CharField(max_length=20, choices=Role_choices)
 
     is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     is_deleted = models.BooleanField(default=False)
     
     created_at = models.DateTimeField(default=timezone.now)
