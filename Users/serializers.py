9ecb7961f0c4edd63a334f6ac729a956b7a1585c
Shubham More <shubhammore0512@gmail.com>
2026-05-26 13:22:17 +0530

    fix the UserSerializers

--- Users/serializers.py
+++ Users/serializers.py
@@ -3,16 +3,33 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True)
+    password = serializers.CharField(write_only=True, required=False)
 
     class Meta:
-        models = User
-        fields = ["id", "email","full_name", "role", "password"]
+        model = User                                    # ← this was missing
+        fields = ["id", "email", "full_name", "role", "business", "password"]
+        read_only_fields = ["id"]
 
-        def create(self, validated_data):
-            password = validated_data.pop("password")
-            user = User.objects.create(**validated_data)
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = User(**validated_data)
+        if password:
             user.set_password(password)
-            user.save()
-            return user
-        
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+
+class ClientUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "full_name", "business"]
+        read_only_fields = ["id", "email", "business"]
